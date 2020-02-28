5fdda1671128053cefc3f789b1a9909092e94aab
Dat Nguyen <lonelyicevalley@icloud.com>
2020-02-28 10:40:05 +0700

    hw2 P3 geometric planning to trajectories and control

--- hw2/P3_traj_planning.py
+++ hw2/P3_traj_planning.py
@@ -1,7 +1,7 @@
 import numpy as np
 from P1_astar import DetOccupancyGrid2D, AStar
 from P2_rrt import *
-import scipy.interpolate
+from scipy.interpolate import splrep, splev
 import matplotlib.pyplot as plt
 from HW1.P1_differential_flatness import *
 from HW1.P2_pose_stabilization import *
@@ -50,7 +50,24 @@ def compute_smoothed_traj(path, V_des, alpha, dt):
     Hint: Use splrep and splev from scipy.interpolate
     """
     ########## Code starts here ##########
-    
+    t = np.zeros(len(path))
+    d_sum = 0
+    path = np.array(path)
+    for i in range(1, len(path)):
+        d = np.linalg.norm(path[i,:]-path[i-1,:])
+        d_sum += d
+        t[i] = d_sum/V_des
+    t_smoothed = np.arange(0,t[-1],dt)
+    tck = splrep(t, path[:,0],s=alpha)
+    x = splev(t_smoothed,tck)
+    x_d = splev(t_smoothed,tck,der=1)
+    x_dd = splev(t_smoothed,tck,der=2)
+    tck = splrep(t, path[:,1],s=alpha)
+    y = splev(t_smoothed,tck)
+    y_d = splev(t_smoothed,tck,der=1)
+    y_dd = splev(t_smoothed,tck,der=2)
+    theta = np.array([np.math.atan2(y_d[i], x_d[i]) for i in range(len(y_d))])
+    traj_smoothed = np.column_stack([x, y, theta, x_d, y_d, x_dd, y_dd])
     ########## Code ends here ##########
 
     return traj_smoothed, t_smoothed
@@ -73,7 +90,14 @@ def modify_traj_with_limits(traj, t, V_max, om_max, dt):
     Hint: This should almost entirely consist of calling functions from Problem Set 1
     """
     ########## Code starts here ##########
-    
+    V, om = compute_controls(traj=traj)
+    s = compute_arc_length(V, t)
+    V_tilde = rescale_V(V, om, V_max, om_max)
+    tau = compute_tau(V_tilde, s)
+    om_tilde = rescale_om(V, om, V_tilde)
+    s_f = State(x=5, y=5, V=V_max, th=-np.pi/2)
+    t_new, V_scaled, om_scaled, traj_scaled = interpolate_traj(
+            traj, tau, V_tilde, om_tilde, dt, s_f)
     ########## Code ends here ##########
 
     return t_new, V_scaled, om_scaled, traj_scaled
