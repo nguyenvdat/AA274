8188ff37880cfad2e8399dbe4d8ef52af531730d
Dat Nguyen <lonelyicevalley@icloud.com>
2020-03-06 18:56:55 +0700

    init hw4

---
+++ hw4/turtlebot_model.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+EPSILON_OMEGA = 1e-3
+
+
+def compute_dynamics(x, u, dt, compute_jacobians=True):
+    """
+    Compute Turtlebot dynamics (unicycle model).
+    Inputs:
+                        x: np.array[3,] - Turtlebot state (x, y, theta).
+                        u: np.array[2,] - Turtlebot controls (V, omega).
+        compute_jacobians: bool         - compute Jacobians Gx, Gu if true.
+    Outputs:
+         g: np.array[3,]  - New state after applying u for dt seconds.
+        Gx: np.array[3,3] - Jacobian of g with respect to x.
+        Gu: np.array[3,2] - Jacobian of g with respect ot u.
+    """
+    ########## Code starts here ##########
+    # TODO: Compute g, Gx, Gu
+
+    ########## Code ends here ##########
+
+    if not compute_jacobians:
+        return g
+
+    return g, Gx, Gu
+
+
+def transform_line_to_scanner_frame(line, x, tf_base_to_camera, compute_jacobian=True):
+    """
+    Given a single map line in the world frame, outputs the line parameters
+    in the scanner frame so it can be associated with the lines extracted
+    from the scanner measurements.
+    Input:
+                     line: np.array[2,] - map line (alpha, r) in world frame.
+                        x: np.array[3,] - pose of base (x, y, theta) in world frame.
+        tf_base_to_camera: np.array[3,] - pose of camera (x, y, theta) in base frame.
+         compute_jacobian: bool         - compute Jacobian Hx if true.
+    Outputs:
+         h: np.array[2,]  - line parameters in the scanner (camera) frame.
+        Hx: np.array[2,3] - Jacobian of h with respect to x.
+    """
+    alpha, r = line
+
+    ########## Code starts here ##########
+    # TODO: Compute h, Hx
+
+    ########## Code ends here ##########
+
+    if not compute_jacobian:
+        return h
+
+    return h, Hx
+
+
+def normalize_line_parameters(h, Hx=None):
+    """
+    Ensures that r is positive and alpha is in the range [-pi, pi].
+    Inputs:
+         h: np.array[2,]  - line parameters (alpha, r).
+        Hx: np.array[2,n] - Jacobian of line parameters with respect to x.
+    Outputs:
+         h: np.array[2,]  - normalized parameters.
+        Hx: np.array[2,n] - Jacobian of normalized line parameters. Edited in place.
+    """
+    alpha, r = h
+    if r < 0:
+        alpha += np.pi
+        r *= -1
+        if Hx is not None:
+            Hx[1, :] *= -1
+    alpha = (alpha + np.pi) % (2*np.pi) - np.pi
+    h = np.array([alpha, r])
+
+    if Hx is not None:
+        return h, Hx
+    return h
